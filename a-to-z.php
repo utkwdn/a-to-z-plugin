27ab534df221869d5483a268b6a30226c9137175
Corbin Schappel <cschappe@utk.edu>
2025-06-03 12:34:36 -0400

    chore(a-to-z-plugin): change author

--- a-to-z.php
+++ a-to-z.php
@@ -6,7 +6,7 @@
  * Version:           0.1.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
- * Author:            UT OCM
+ * Author:            The University of Tennessee, Knoxville
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       a-to-z
