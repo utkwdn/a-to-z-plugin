f1f8aad427a2ef2b29a260fd0ca7801a02883127
Mark Naylor <marknaylor@MacBookPro.attlocal.net>
2025-06-30 15:25:03 -0500

    feat(plugin-update-checker): add plugin update checker, update formatting

--- src/a-to-z/render.php
+++ src/a-to-z/render.php
@@ -3,10 +3,10 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
- if (!function_exists('a_to_z_render_callback')) {
-    function a_to_z_render_callback() {
-        return '<div class="a-to-z-container alignfull" id="alpha"></div>';
-    }
+if ( ! function_exists( 'a_to_z_render_callback' ) ) {
+	function a_to_z_render_callback() {
+		return '<div class="a-to-z-container alignfull" id="alpha"></div>';
+	}
 }
 
 // Output the rendered HTML
