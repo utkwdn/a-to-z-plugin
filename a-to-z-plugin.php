c290a69628c97af7c88b5e71ae67850cfd90c740
Mark Naylor <marknaylor@MacBookPro.attlocal.net>
2025-07-03 15:07:24 -0500

    chore(entry): revert version number

--- a-to-z-plugin.php
+++ a-to-z-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       A to Z
  * Plugin URI:        https://www.utk.edu
  * Description:       A plugin for managing and displaying the A to Z index
- * Version:           0.1.1
+ * Version:           0.1.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The University of Tennessee, Knoxville
